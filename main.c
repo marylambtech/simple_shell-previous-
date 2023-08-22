54d0c4702fa60ccc92c3943e50ff15bd3360c3e4
marylambtech <marylambtech@gmail.com>
2023-08-22 04:06:12 -0700

    shell.h

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include “shell.h”
+
+/**
+ *sum - adds two integers
+ *@n1: 1st integer
+ *@n2: 2nd integer
+ *Return: sum
+*/
+
+int sum(int n1, int n2)
+{
+	return (n1 + n2);
+}
---
+++ shell.h
@@ -0,0 +1,10 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int sum(int n1, int n2);
+
+#endif
