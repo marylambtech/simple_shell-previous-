f9c32348d5d8bf768dba0544ab098404363e14fe
jsktk <joshuaakototamakloe@gmail.com>
2023-08-23 12:43:40 -0700

    simple_shell.c

---
+++ simple_shell.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+/**
+* main - main
+* @argc: Argument 1
+* @argv: Argument 2
+* @env: environment variable
+*
+* Return: always 0
+*/
+
+int main(int argc, char **argv, char **env)
+{
+	if (argc == 1)
+		prompt(av, env);
+	return (0);
+}
